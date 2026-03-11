fabdddf68fd7ca0d54085e1cffd7bdb0e9be2c14
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-03-11 11:16:09 -0300

    feat: add order item pk

---
+++ src/main/java/com/projeto/ecommerce/entities/OrderItemPK.java
@@ -0,0 +1,25 @@
+package com.projeto.ecommerce.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class OrderItemPK {
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private OrderEntity order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductEntity product;
+
+}
