490adbfdb0323047a6acec8c3c5b797d296e381f
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-03-11 11:18:23 -0300

    feat: add user response dto constructor

--- src/main/java/com/projeto/ecommerce/responses/UserResponseDTO.java
+++ src/main/java/com/projeto/ecommerce/responses/UserResponseDTO.java
@@ -1,5 +1,6 @@
 package com.projeto.ecommerce.responses;
 
+import com.projeto.ecommerce.entities.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,11 +9,24 @@ import java.util.UUID;
 
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class UserResponseDTO {
     private UUID id;
     private String name;
     private String email;
     private String phone;
+
+    public UserResponseDTO(UUID id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public UserResponseDTO(UserEntity client) {
+        this.id = client.getId();
+        this.name = client.getName();
+        this.email = client.getEmail();
+        this.phone = client.getPhone();
+    }
 }
