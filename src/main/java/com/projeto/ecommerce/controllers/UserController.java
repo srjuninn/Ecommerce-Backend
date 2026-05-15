90c5a449a99665adb998de0a8a6cbe8bef1b466a
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-15 11:04:13 -0300

    feat: add photo service method

--- src/main/java/com/projeto/ecommerce/controllers/UserController.java
+++ src/main/java/com/projeto/ecommerce/controllers/UserController.java
@@ -2,11 +2,15 @@ package com.projeto.ecommerce.controllers;
 
 import com.projeto.ecommerce.requests.UserRequestDTO;
 import com.projeto.ecommerce.responses.UserResponseDTO;
+import com.projeto.ecommerce.services.PhotoService;
 import com.projeto.ecommerce.services.UserService;
 import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,22 +18,31 @@ import java.util.UUID;
 @RequestMapping("user")
 public class UserController {
     private final UserService userService;
+    private final PhotoService photoService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService, PhotoService photoService){
         this.userService = userService;
+        this.photoService = photoService;
     }
 
-    @PostMapping("create")
-    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO userReq){
-        UserResponseDTO userRes = userService.createUser(userReq);
+    @PostMapping(
+            value = "/show/all",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<UserResponseDTO> createUser(@RequestParam String name, @RequestParam String email,@RequestParam String phone,@RequestParam String password   , @RequestParam MultipartFile photo) throws IOException {
+        String pathPhoto = photoService.savePhoto(photo);
+        UserRequestDTO userReq = new UserRequestDTO(name, email, phone, password, pathPhoto);
+        UserResponseDTO userRes = userService.createUser(userReq, pathPhoto);
         return ResponseEntity.ok(userRes);
     }
+
+
     @GetMapping("/show/{id}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable UUID id) {
         UserResponseDTO response = userService.getUserById(id);
         return ResponseEntity.ok(response);
     }
-    @GetMapping("/show/all")
+    @GetMapping("/show/alll")
     public ResponseEntity<List<UserResponseDTO>> getAllUsers(){
         List<UserResponseDTO> userRes = userService.getAllUsers();
         return ResponseEntity.ok(userRes);
